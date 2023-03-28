24c6a3a73597424fb2a89ed0a126135f4919e594
Boxuan-Wang <74303110+Boxuan-Wang@users.noreply.github.com>
2023-03-28 12:05:09 +0800

    Add files via upload

---
+++ drive_download.sh
@@ -0,0 +1,11 @@
+pip install gdown
+mkdir models
+cd models
+# The latest model for rows
+gdown "https://drive.google.com/drive/folders/1ltjVVn21GxaTeJko6I-JDb-Y_C6XX1Wg?usp=sharing" --folder
+# the latest model for cols
+gdown "https://drive.google.com/drive/folders/1-TttPo3MBOq8beYLIutgdYjVBFpRsWOz?usp=sharing" --folder
+# the latest model for cells
+gdown "https://drive.google.com/drive/folders/1VKOWc7ujhHoe_veqWZ-MGRM3lFgLwf25?usp=sharing" --folder
+# get the trained model for Lattice 
+gdown "https://drive.google.com/file/d/1Jgo1KIrYh2hRKT3CLimgqYw0kHx_SOWf/view?usp=sharing" --fuzzy
