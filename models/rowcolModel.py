4efb56d5ff95d5b14aead2d3b70af18283f15107
Boxuan Wang <bw447@cam.ac.uk>
2023-04-06 11:34:59 +0800

    fix indent

--- models/rowcolModel.py
+++ models/rowcolModel.py
@@ -47,9 +47,9 @@ class ColRowClassifier(pl.LightningModule):
     return loss
   
   def predict(self, x):
-		logits = self.forward(x)
-		m = torch.nn.Softmax(dim=1)
-		return m(logits).detach().numpy()[0][0]
+    logits = self.forward(x)
+    m = torch.nn.Softmax(dim=1)
+    return m(logits).detach().numpy()[0][0]
  
 def tokenize(text:str, tokenizer:BertTokenizer) -> Tensor:
   return tokenizer(
