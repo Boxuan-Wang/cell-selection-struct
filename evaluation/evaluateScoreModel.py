6cb1acc1d42ca55c76cacbfcae7c05abc333026b
Boxuan Wang <bw447@cam.ac.uk>
2023-04-11 18:41:51 +0800

    evaluation

--- evaluation/evaluateScoreModel.py
+++ evaluation/evaluateScoreModel.py
@@ -91,36 +91,36 @@ def evaluate_score_model(
                     output_file.write(json.dumps(result_dict,ensure_ascii=False) + "\n")
     print("Finish evaluation.")
                                  
-if __name__ == "__main__":
-    # todo: add arg parser to the function
-    parser = argparse.ArgumentParser(description="The function to evaluate cell selection model.")
-    parser.add_argument('--test_path', '-t', type=str, help="The path to test dataset.")
-    parser.add_argument('--store_path', '-s', type=str, help='Path to store the evaluation result')
-    parser.add_argument('--db_path', '-d', type=str, help='The path to WIKI database.')
-    parser.add_argument('--ttt_path',type=str, help='The path to table-to-text generation.')
-    parser.add_argument('--row_score','-r', type=str, help='The path to scoring rows')
-    parser.add_argument('--min_score', type=float, default=0.05, help='The minimum score of a relevant cell')
-    parser.add_argument('--max_num_relevant',default=10, type=int, help='Max number of relevant cells in a table')
-    parser.add_argument('--col_score','-c',type=str,help='The path to scoring column model')
-    parser.add_argument('--max_table_size', '-m', default=400, type=int, help='Maximum size of table the model allows')
-    parser.add_argument('--pretesting', '-p', default=False, action='store_true')
-    args = parser.parse_args()
+# if __name__ == "__main__":
+#     # todo: add arg parser to the function
+#     parser = argparse.ArgumentParser(description="The function to evaluate cell selection model.")
+#     parser.add_argument('--test_path', '-t', type=str, help="The path to test dataset.")
+#     parser.add_argument('--store_path', '-s', type=str, help='Path to store the evaluation result')
+#     parser.add_argument('--db_path', '-d', type=str, help='The path to WIKI database.')
+#     parser.add_argument('--ttt_path',type=str, help='The path to table-to-text generation.')
+#     parser.add_argument('--row_score','-r', type=str, help='The path to scoring rows')
+#     parser.add_argument('--min_score', type=float, default=0.05, help='The minimum score of a relevant cell')
+#     parser.add_argument('--max_num_relevant',default=10, type=int, help='Max number of relevant cells in a table')
+#     parser.add_argument('--col_score','-c',type=str,help='The path to scoring column model')
+#     parser.add_argument('--max_table_size', '-m', default=400, type=int, help='Maximum size of table the model allows')
+#     parser.add_argument('--pretesting', '-p', default=False, action='store_true')
+#     args = parser.parse_args()
     
-    db_connection = WIKI_connection(args.db_path)
-    score_classifier = ScoreClassifier(
-        args.row_score,
-        args.col_score, 
-        db_connection,
-        minimum_score=args.min_score,
-        relevant_num_limit= args.max_num_relevant
-        )
-    ttt_model = TextGenerater(args.ttt_path)
+#     db_connection = WIKI_connection(args.db_path)
+#     score_classifier = ScoreClassifier(
+#         args.row_score,
+#         args.col_score, 
+#         db_connection,
+#         minimum_score=args.min_score,
+#         relevant_num_limit= args.max_num_relevant
+#         )
+#     ttt_model = TextGenerater(args.ttt_path)
     
-    evaluate_score_model(
-        args.test_path,
-        args.store_path,
-        score_classifier,
-        ttt_model,
-        db_connection,
-        args.max_table_size,
-        pretesting=args.pretesting)
+#     evaluate_score_model(
+#         args.test_path,
+#         args.store_path,
+#         score_classifier,
+#         ttt_model,
+#         db_connection,
+#         args.max_table_size,
+#         pretesting=args.pretesting)
